f89de63f1461b7eed3771e01a6ffbfee9daafe96
Miguel <mcordero@colegioelbelloto.com>
2025-05-22 15:24:20 -0400

    Cuarto commit

--- parte3/2.js
+++ parte3/2.js
@@ -1,34 +1,32 @@
 // Función tradicional
 
 function  celsiusToFahrenheit(c){
-    c = Number(prompt("Ingrese grados celsius"));
+    c = Number(prompt("Ingrese grados celsius | Función tradicional"));
     return (c * 9/5) + 32;
 }
+console.log("Función tradicional | Grados celsius a fahrenheit: " + celsiusToFahrenheit());
 
 // Función flecha
 
 let celsiusHaciaFahrenheit = (c) => {
-    c = Number(prompt("Ingrese grados celsius"));
+    c = Number(prompt("Ingrese grados celsius | Función flecha"));
     return (c * 9/5) + 32;
 }
-
-celsiusHaciaFahrenheit(25)
+console.log("Función flecha | Grados celsius a fahrenheit: " + celsiusHaciaFahrenheit());
 
 // Función tradicional
 
 function fahrenheitToCelsius(f){
-    f = (Number(prompt("Ingrese grados fahrenheit")));
+    f = (Number(prompt("Ingrese grados fahrenheit | Función tradicional")));
     return (f - 32) * 5/9;
 }
+console.log("Función tradicional | Grados fahrenheit a celsius: " + fahrenheitToCelsius());
 
 // Función flecha
 
 let fahrenheitHaciaCelsius = (f) => {
-    f = (Number(prompt("Ingrese grados fahrenheit")));
+    f = (Number(prompt("Ingrese grados fahrenheit | Función flecha")));
     return (f - 32) * 5/9;
 }
 
-fahrenheitHaciaCelsius(68)
-
-console.log("Grados celsius a fahrenheit: " + celsiusToFahrenheit());
-console.log("Grados fahrenheit a celsius: " + fahrenheitToCelsius());
+console.log("Función flecha | Grados fahrenheit a celsius: " + fahrenheitHaciaCelsius());
--- parte3/3.js
+++ parte3/3.js
@@ -1,13 +1,17 @@
 // Función tradicional
 function TotalAPagar(monto, propina){
-    return "Propina: " + propina + " Monto total a pagar: " + (monto + propina);
+    monto = Number(prompt("Ingrese el monto a pagar | Función tradicional"));
+    propina = Number(prompt("Ingrese el monto de la propina | Función tradicional"));
+    return "Función tradicional | Propina: " + propina + " | Monto total a pagar: " + (monto + propina);
 
 }
+console.log(TotalAPagar());
 
 // Función flecha
 
 let totalPagar = (monto, propina) => {
-    return "Propina: " + propina + " Monto total a pagar: " + (monto + propina);
+    monto = Number(prompt("Ingrese el monto a pagar | Función flecha"));
+    propina = Number(prompt("Ingrese el monto de la propina | Función flecha"));
+    return "Función flecha | Propina: " + propina + " | Monto total a pagar: " + (monto + propina);
 }
-
-totalPagar(10000, 3000);
+console.log(totalPagar());
